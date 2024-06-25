bb50acca76502b82d70a4be64f081688595008b5
Emir <ebasic@gmail.com>
2024-06-25 13:33:54 +0200

    feature: add suspense with pokedex component

---
+++ playground/src/components/pokedex.vue
@@ -0,0 +1,26 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script>
+// https://pokeapi.co/api/v2/pokemon?limit=151
+
+export default {
+  async setup() {
+    const regionName = 'kanto'
+    const pokedex = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151').then(response =>
+      response.json()
+    )
+
+    return {
+      pokedex,
+      regionName
+    }
+  },
+  created() {
+    console.log(this.regionName)
+    console.log(this.pokedex)
+  }
+}
+</script>
+<template>
+  <h2>{{ regionName }}</h2>
+  <pre>{{ pokedex }}</pre>
+</template>
