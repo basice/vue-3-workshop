f8335ef44837af789491d779b42a8d9068931ad0
Emir <ebasic@gmail.com>
2024-06-25 20:39:42 +0200

    feature: add script setup example in playground

--- playground/src/components/pokedex.vue
+++ playground/src/components/pokedex.vue
@@ -1,43 +1,39 @@
 <!-- eslint-disable vue/multi-word-component-names -->
-<script>
-// https://pokeapi.co/api/v2/pokemon?limit=151
+<script setup>
+import BaseButton from './base-button.vue'
+import { computed, defineProps, defineEmits, ref, reactive } from 'vue'
 
-import { computed, ref, reactive } from 'vue'
+const emits = defineEmits(['change-region'])
 
-export default {
-  async setup() {
-    const regionName = ref('Kanto')
-
-    const state = reactive({
-      elementType: 'lightning'
-    })
-
-    console.log(regionName)
-    console.log(state.elementType)
-
-    const elementTypeAllCaps = computed(() => {
-      return state.elementType.toUpperCase()
-    })
-
-    const pokedex = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151').then(response =>
-      response.json()
-    )
-
-    const changeRegionName = () => {
-      regionName.value = 'Hoenn'
-    }
-
-    return {
-      changeRegionName,
-      elementTypeAllCaps,
-      pokedex,
-      regionName
-    }
+const props = defineProps({
+  region: {
+    type: String
   }
+})
+
+const regionName = ref('Kanto')
+
+const state = reactive({
+  elementType: 'lightning'
+})
+
+const elementTypeAllCaps = computed(() => {
+  return state.elementType.toUpperCase() + props.region
+})
+
+const pokedex = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151').then(response =>
+  response.json()
+)
+
+const changeRegionName = () => {
+  regionName.value = 'Hoenn'
+  emits('change-region')
 }
 </script>
+
 <template>
   <h2>{{ regionName }}</h2>
+  <BaseButton />
   <h3>{{ elementTypeAllCaps }}</h3>
   <button @click="changeRegionName">Change Region Name</button>
   <pre>{{ pokedex }}</pre>
