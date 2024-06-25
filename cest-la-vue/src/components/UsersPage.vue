d2a221d1410bf41cb33779c5431ad50493119a0b
Emir <ebasic@gmail.com>
2024-06-25 20:57:04 +0200

    refactor: use script setup for userpage

--- cest-la-vue/src/components/UsersPage.vue
+++ cest-la-vue/src/components/UsersPage.vue
@@ -1,36 +1,33 @@
-<script>
+<script setup>
 import UserCard from './UserCard.vue'
 import { reactive } from 'vue'
 
-export default {
-  components: {
-    UserCard
-  },
-  async setup() {
-    const state = reactive({
-      userList: []
-    })
-
-    async function fetchUsers() {
-      const response = await fetch('https://jsonplaceholder.typicode.com/users').then((response) =>
-        response.json()
-      )
-      return response
-    }
-
-    state.userList = await fetchUsers()
-
-    return {
-      state,
-      fetchUsers
-    }
+defineProps({
+  title: {
+    type: String,
+    default: 'Users'
   }
+})
+
+defineEmits(['update-user-list'])
+
+const state = reactive({
+  userList: []
+})
+
+async function fetchUsers() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users').then((response) =>
+    response.json()
+  )
+  return response
 }
+
+state.userList = await fetchUsers()
 </script>
 
 <template>
   <main>
-    <h1>Users</h1>
+    <h1>{{ title }}</h1>
     <ul>
       <UserCard
         v-for="user in state.userList"
